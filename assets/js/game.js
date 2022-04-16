04bf44695df0232e3938ec270575be806e319030
Zach Burkhart <zachburkhart216@gmail.com>
2022-04-16 01:04:39 -0400

    added round numbers

--- assets/js/game.js
+++ assets/js/game.js
@@ -66,7 +66,13 @@ var fight = function(enemyName) {
   }; // end of fight function
 
 for(var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    fight(pickedEnemyName);
+    if (playerHealth > 0) {
+        window.alert("Welcome to Robot Gladiators!");
+        var pickedEnemyName = enemyNames[i];
+        enemyHealth = 50;
+        fight(pickedEnemyName);
+      } else {
+        window.alert("You have lost your robot in battle! Game Over!");
+        break;
+      }
 }
